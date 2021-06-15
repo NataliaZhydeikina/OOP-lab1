1c4077448f24ef4a739424c845bdfad81ca6486e
NataliaZhydeikina <nata.iiinx88@gmail.com>
2021-06-15 15:35:30 +0300

    lab1

---
+++ ConsoleAppLab1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using GeometryLib;
+
+namespace ConsoleAppLab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Line line1 = new Line(-1, 1, -2, 2);
+            Console.WriteLine("Line distance: " + line1.length());
+            Console.WriteLine("Angle between a line and the x-axis: " + line1.getAngleOX());
+            Console.WriteLine("Angle between a line and the y-axis: "+line1.getAngleOY());
+
+            Line line2 = new Line(-7, -5, -12, 45);
+            Console.WriteLine("Line2 distance: " + line2.length());
+            Console.WriteLine("Angle between a line2 and the x-axis: " + line2.getAngleOX());
+            Console.WriteLine("Angle between a line2 and the y-axis: " + line2.getAngleOY());
+        }
+    }
+}
---
+++ GeometryLib/Line.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace GeometryLib
+{
+    public class Line
+    {
+        public float x1 { get; set; }
+        public float y1 { get; set; }
+        public float x2 { get; set; }
+        public float y2 { get; set; }
+
+        public Line(float x1, float y1, float x2, float y2)
+        {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+        }
+
+        public float[] getCoordinates() {
+            return new float[] {x1,y1,x2,y2};
+        }
+
+        private double getWidth() { 
+            return x2 - x1; ;
+        }
+        private double getHeight()
+        {
+            return y2 - y1;
+        }
+        public double length() {
+            double w = getWidth(); 
+            double h = getHeight();
+            double len = Math.Sqrt(Math.Pow(w, 2) + Math.Pow(h, 2));
+            return Math.Round(len, 2);
+        }
+
+        public double getAngleOX() {
+            double w = getWidth();
+            double angle = Math.Acos(w / length()) * (180 / Math.PI);
+            return  Math.Round(angle, 2);
+        }
+
+        public double getAngleOY()
+        {
+            double h = getHeight();
+            double angle = Math.Acos(h / length()) * (180 / Math.PI);
+            return Math.Round(angle, 2);
+        }
+    }
+}
